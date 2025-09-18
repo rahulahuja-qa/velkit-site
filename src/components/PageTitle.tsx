d6dd96fcd53793828798592235f4153057f8bcf2
Rahul Ahuja <52635806+rahulahuja-qa@users.noreply.github.com>
2025-09-18 13:23:44 +1200

    remove cs50

--- src/components/PageTitle.tsx
+++
@@ -1,9 +0,0 @@
-export default function PageTitle({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="border-b bg-gray-50">
-      <div className="mx-auto max-w-5xl p-4">
-        <h1 className="text-xl font-semibold">{children}</h1>
-      </div>
-    </div>
-  );
-}
